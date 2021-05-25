45366821a5e406c6f6a2deda83ec3fc2ca2b84fd
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-05-25 20:47:28 +0600

    adding navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,9 +6,12 @@ const Header = () => {
 
 
 return (
+  <React.Fragment>
 <div className="text">
   <div>
   <h1 style={{color:'#38aeab'}}>HODLINFO<span style={{fontSize:'20px',color:'white'}}>.com</span></h1>
+  <br />
+  <p style={{color:'gray'}}>Powered by <span style={{color:'#38aeab'}}>Finstreet</span></p>
   </div>
   <div>
     <div class="dropdown">
@@ -30,9 +33,10 @@ return (
     </div>
     
     <div style={{display:'flex'}}>
-      <p>58</p>
-      <p>Connect Telegram</p>
-      <div className='custom-control custom-switch'>
+      <p className="number-p">58</p>
+      <p className="text-p">Connect Telegram</p>
+
+      <div style={{marginRight:'10px',marginTop:'10px'}} className='custom-control custom-switch'>
         <input
           type='checkbox'
           className='custom-control-input'
@@ -43,10 +47,20 @@ return (
           
         </label>
       </div>
+
     </div>
 
 
   </div>
+  <div className="view-bar">
+    <h5>#</h5>
+    <h5>Name</h5>
+    <h5>Last</h5>
+    <h5>Buy/sell price</h5>
+    <h5>volume</h5>
+    <h5>base_unit</h5>
+  </div>
+  </React.Fragment>
   );
   };
 
