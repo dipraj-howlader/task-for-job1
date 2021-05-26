242c187b2ca65c7459f4ec7d352767c88236d99e
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-05-26 20:36:51 +0600

    finally finish

--- src/Components/Currency/Currency.js
+++ src/Components/Currency/Currency.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import ExplainCurrency from '../ExplainCurrency/ExplainCurrency';
 import './Currency.css';
 
 const Currency = () => {
@@ -15,12 +14,9 @@ const Currency = () => {
         .then(res => res.json())
         .then(data => setCyptocurrency(data))
     }, [])
-        const first = [1,2,3,4,5,6,7,8,9,10];
-        const second  = [1,2,3,4,5,6];
         
         if(cyptocurrency?.batinr){
             var { batinr,btcinr,eosinr,ethinr,iostinr,omginr,trxinr,xrpinr,zilinr,zrxinr } = cyptocurrency;
-            // console.log(cyptocurrency)
         }
 
 
@@ -29,8 +25,8 @@ const Currency = () => {
             <div className="curr-style">
                 <p>1</p>
                 <p>{btcinr?.name}</p>
-                <p>{btcinr?.last}</p>
-                <p>{btcinr?.buy}/{btcinr?.sell}</p>
+                <p>₹{btcinr?.last}</p>
+                <p>₹{btcinr?.buy}/₹{btcinr?.sell}</p>
                 <p>{btcinr?.volume}</p>
                 <p>{btcinr?.base_unit}</p>
                
@@ -38,72 +34,72 @@ const Currency = () => {
             <div className="curr-style">
             <p>2</p>
                 <p>{batinr?.name}</p>
-                <p>{batinr?.last}</p>
-                <p>{batinr?.buy}/{batinr?.sell}</p>
+                <p>₹{batinr?.last}</p>
+                <p>₹{batinr?.buy}/₹{batinr?.sell}</p>
                 <p>{batinr?.volume}</p>
                 <p>{batinr?.base_unit}</p>
             </div>
             <div className="curr-style">
             <p>3</p>
                 <p>{eosinr?.name}</p>
-                <p>{eosinr?.last}</p>
-                <p>{eosinr?.buy}/{eosinr?.sell}</p>
+                <p>₹{eosinr?.last}</p>
+                <p>₹{eosinr?.buy}/₹{eosinr?.sell}</p>
                 <p>{eosinr?.volume}</p>
                 <p>{eosinr?.base_unit}</p>
             </div>
             <div className="curr-style">
             <p>4</p>
                 <p>{ethinr?.name}</p>
-                <p>{ethinr?.last}</p>
-                <p>{ethinr?.buy}/{ethinr?.sell}</p>
+                <p>₹{ethinr?.last}</p>
+                <p>₹{ethinr?.buy}/₹{ethinr?.sell}</p>
                 <p>{ethinr?.volume}</p>
                 <p>{ethinr?.base_unit}</p>
             </div>
             <div className="curr-style">
             <p>5</p>
                 <p>{iostinr?.name}</p>
-                <p>{iostinr?.last}</p>
-                <p>{iostinr?.buy}/{iostinr?.sell}</p>
+                <p>₹{iostinr?.last}</p>
+                <p>₹{iostinr?.buy}/₹{iostinr?.sell}</p>
                 <p>{iostinr?.volume}</p>
                 <p>{iostinr?.base_unit}</p>
             </div>
             <div className="curr-style">
             <p>6</p>
                 <p>{omginr?.name}</p>
-                <p>{omginr?.last}</p>
-                <p>{omginr?.buy}/{omginr?.sell}</p>
+                <p>₹{omginr?.last}</p>
+                <p>₹{omginr?.buy}/₹{omginr?.sell}</p>
                 <p>{omginr?.volume}</p>
                 <p>{omginr?.base_unit}</p>
             </div>
             <div className="curr-style">
             <p>7</p>
                 <p>{trxinr?.name}</p>
-                <p>{trxinr?.last}</p>
-                <p>{trxinr?.buy}/{trxinr?.sell}</p>
+                <p>₹{trxinr?.last}</p>
+                <p>₹{trxinr?.buy}/₹{trxinr?.sell}</p>
                 <p>{trxinr?.volume}</p>
                 <p>{trxinr?.base_unit}</p>
             </div>
             <div className="curr-style">
             <p>8</p>
                 <p>{xrpinr?.name}</p>
-                <p>{xrpinr?.last}</p>
-                <p>{xrpinr?.buy}/{xrpinr?.sell}</p>
+                <p>₹{xrpinr?.last}</p>
+                <p>₹{xrpinr?.buy}/₹{xrpinr?.sell}</p>
                 <p>{xrpinr?.volume}</p>
                 <p>{xrpinr?.base_unit}</p>
             </div>
             <div className="curr-style">
             <p>9</p>
                 <p>{zilinr?.name}</p>
-                <p>{zilinr?.last}</p>
-                <p>{zilinr?.buy}/{zilinr?.sell}</p>
+                <p>₹{zilinr?.last}</p>
+                <p>₹{zilinr?.buy}/₹{zilinr?.sell}</p>
                 <p>{zilinr?.volume}</p>
                 <p>{zilinr?.base_unit}</p>
             </div>
             <div className="curr-style">
             <p>10</p>
                 <p>{zrxinr?.name}</p>
-                <p>{zrxinr?.last}</p>
-                <p>{zrxinr?.buy}/{zrxinr?.sell}</p>
+                <p>₹{zrxinr?.last}</p>
+                <p>₹{zrxinr?.buy}/₹{zrxinr?.sell}</p>
                 <p>{zrxinr?.volume}</p>
                 <p>{zrxinr?.base_unit}</p>
             </div>
